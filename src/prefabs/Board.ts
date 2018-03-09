55e0879b52b802336193940469d709ace06f0289
Craig Kaminski <craig.j.kaminski@gmail.com>
2018-03-09 06:29:26 -0500

    Base tile image on data in map.jon

--- src/prefabs/Board.ts
+++ src/prefabs/Board.ts
@@ -1,10 +1,22 @@
 import { Game } from '../states/Game';
 
+interface ITerrain {
+  asset: string;
+  blocked?: boolean;
+}
+
 export class Board extends Phaser.Group {
   private cols: number;
   private grid: number[][];
   private rows: number;
   private state: Game;
+  private readonly terrains: ITerrain[] = [
+    { asset: 'grass' },
+    { asset: 'water', blocked: true },
+    { asset: 'rocks' },
+    { asset: 'grasstrees' },
+    { asset: 'grasstrees2' },
+  ];
 
   constructor(state: Game, grid: number[][]) {
     super(state.game);
@@ -25,7 +37,16 @@ export class Board extends Phaser.Group {
 
         const y = this.state.MARGIN_Y + row * this.state.TILE_H * 3 / 4;
 
-        const tile = new Phaser.Sprite(this.game, x, y, 'grass');
+        const tile = new Phaser.Sprite(this.game, x, y, this.terrains[this.grid[row][col]].asset);
+
+        tile.data.row = row;
+        tile.data.col = col;
+        tile.data.terrainAsset = this.terrains[this.grid[row][col]].asset;
+        tile.data.blocked = this.terrains[this.grid[row][col]].blocked;
+
+        tile.inputEnabled = true;
+        tile.input.pixelPerfectClick = true;
+
         this.add(tile);
       }
     }
